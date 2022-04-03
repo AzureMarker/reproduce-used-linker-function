df05ee1c7b0924372a7b03fbf74d15aedb3d2f96
Mark Drobnak <mark.drobnak@gmail.com>
2022-04-03 16:51:42 -0700

    Add init function workaround

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 extern crate pretend_std;
 
-mod c_impl {
+pub mod c_impl {
+    /// Call this somewhere to force Rust to link this module.
+    /// The call doesn't need to execute, just exist.
+    ///
+    /// See https://github.com/rust-lang/rust/issues/47384
+    pub fn init() {}
+
     #[no_mangle]
     pub extern "C" fn exported_function() -> libc::c_int {
         42
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
+    // Uncomment this line to fix linking.
+    // Related to https://github.com/rust-lang/rust/issues/47384
+    // reproduce_used_linker_function::c_impl::init();
+
     println!("output: {}", reproduce_used_linker_function::value());
 }
